819634bd0e9e8a5f19d7a4d790ac6b331ff8510d
Gavin Donoho <gavin@litenlite.com>
2024-09-14 14:43:44 -0700

    initial creation of swift project

---
+++ xcode/IValU/IValU/IValUApp.swift
@@ -0,0 +1,17 @@
+//
+//  IValUApp.swift
+//  IValU
+//
+//  Created by Gavin Donoho on 9/14/24.
+//
+
+import SwiftUI
+
+@main
+struct IValUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
