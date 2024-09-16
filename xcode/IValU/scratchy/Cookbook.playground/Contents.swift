454e6c32356b74a41ccc575c9d64c9c79ee472da
Gavin Donoho <gavin@litenlite.com>
2024-09-15 20:39:56 -0700

    incremental check in - listening to War 3

---
+++ xcode/IValU/scratchy/Cookbook.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var greeting = "Hello, Lightning lover!"
+greeting = "go away now, please"
+let phrase: String = "We have the answer to all of your questions"
+let numberOfAnswers: Int = 1
+let numberOfQuestions: Int = 2
+
+/*
+ put whoops here
+ */
+
+var phraseinfo = "\(numberOfQuestions) questions ended up having \(numberOfAnswers) answer"
